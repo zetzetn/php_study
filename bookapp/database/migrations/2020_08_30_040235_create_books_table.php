9d4b171933af81f7e2434d748de470e0370b8666
harmony.is.valued@gmail.com <53105202+zetzetn@users.noreply.github.com>
2020-08-30 16:18:12 +0900

    database,マイグレーションファイル修正

--- bookapp/database/migrations/2020_08_30_040235_create_books_table.php
+++ bookapp/database/migrations/2020_08_30_040235_create_books_table.php
@@ -14,7 +14,8 @@ class CreateBooksTable extends Migration
     public function up()
     {
         Schema::create('books', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('title');        
             $table->timestamps();
         });
     }
