dd3cc29a401e24aa4bb50cbcfb8d82665047881d
harmony.is.valued@gmail.com <53105202+zetzetn@users.noreply.github.com>
2020-08-30 20:18:24 +0900

    バックアップ作成

---
+++ bookapp.org/routes/web.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Book;
+// use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $books = Book ::all();
+    return view('books' , ['books' => $books ]);
+});
+
+Route::post('/book', function(Request $request) {
+    $validator = Validator::make($request->all(),[
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);    
+    } 
+
+    $book = new Book;
+    $book->title = $request->name;
+    $book->save();
+
+    return redirect('/');
+});
+
+Route::delete('/book/{book}',function(Book $book){
+    $book->delete();
+
+    return redirect('/');
+});
+
+
