0481b8328eda9ed2e59dd320dd2903472ca0d2f2
harmony.is.valued@gmail.com <53105202+zetzetn@users.noreply.github.com>
2020-08-30 19:10:56 +0900

    投稿データの保存処理を追加

--- bookapp/routes/web.php
+++ bookapp/routes/web.php
@@ -18,3 +18,15 @@ Route::get('/', function () {
     $books = Book ::all();
     return view('books' , ['books' => $books ]);
 });
+
+Route::post('/book', function(Request $request) {
+    $validator = Validator::make($request->all(),[
+        'name' => 'required|max:255',
+    ]);
+
+    $book = new Book;
+    $book->title = $request->name;
+    $book->save();
+
+    return redirect('/');
+});
